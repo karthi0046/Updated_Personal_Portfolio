c5f0d69bb0174f79cff83bc158fa333158e6eb3b
RAMKARTHICK S <123952399+karthi0046@users.noreply.github.com>
2026-01-11 13:09:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+    // function closeSidebar() {
+    //     const sidebar = document.getElementById("sidebar");
+    //     const baricon = document.getElementById("baricon");
+
+    //     sidebar.classList.remove("active"); // Remove active class to hide sidebar
+    //     baricon.style.display = "block"; // Show bar icon after closing sidebar
+    // }
+
+    // function openSidebar() {
+    //     const sidebar = document.getElementById("sidebar");
+    //     const baricon = document.getElementById("baricon");
+
+    //     sidebar.classList.add("active"); // Add active class to show sidebar
+    //     baricon.style.display = "none"; // Hide bar icon when sidebar is open
+    // }
+
+    function openSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.add("active");
+}
+
+function closeSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.remove("active");
+}
+
+
+
